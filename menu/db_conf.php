c78fb05d94a603a063323d36fd11ab405fcdc444
Pavel Timerhanov <pavel.timerhanov@khk.ee>
2019-02-07 13:22:55 +0200

    Muudan index.php ja db_sql

--- menu/db_conf.php
+++ menu/db_conf.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', '');
-define('DB_PASS', '');
-define('DB_NAME', '');
+define('DB_USER', 'paveltimerhanovi');
+define('DB_PASS', 'E^Ei;&+_vyqS');
+define('DB_NAME', 'paveltim_php');
 ?>
--- menu/index.php
+++ menu/index.php
@@ -1,16 +1,6 @@
 <?php
-require_once 'lib/fnk.php';
-require_once 'menus/menu.php';
-// lehe sisu
-// päis - header ja muud vajalikud osad
-pais();
-// menüü elemendid - menu funktsioon kasutab menu_element funktsiooni sisu loomiseks
-menu(array(
-    'praed' => $praed,
-    'supid' => $supid,
-    'magus' => $magusad,
-    'joogid' => $joogid
-));
-// jalus - kõik JS asjad
-jalus();
+require_once 'conf.php';
+echo '<pre>';
+print_r($sess);
+echo '</pre>';
 ?>
