dd601bdb93df548ca863722bf0846a9f85a1fa5f
Pavel Timerhanov <pavel.timerhanov@khk.ee>
2019-02-21 09:44:15 +0200

    21.02.2019

--- menu/index.php
+++ menu/index.php
@@ -9,60 +9,23 @@ echo '</pre>';*/
 $mainTmpl = new Template('main');
 $mainTmpl->set('title', 'Menu Application');
 $contentTmpl = new Template('content');
-$courseCardTmpl = new Template('course_card');
-$courseCardHeaderTmpl = new Template('course_card_header.html');
-$courseCardDataTmpl = new Template('course_card_data');
-$courseDatalistTmpl = new Template('course_data_list');
-
-$courses = array(
-    array(
-        'name' => 'praed',
-        'icon' => 'fa-utensils',
-        'data' => array(
-            array(
-                'dish_name' => 'Sealihapada ploomide ja aprikoosiga',
-                'dish_description' => 'sealihapada, lisand, salat, leib',
-                'dish_price' => 2.65,
-                'discount' => 2.25
-            ),
-            array(
-                'dish_name' => 'Praetud kanakints',
-                'dish_description' => 'praetud kana, lisand, salat, leib',
-                'dish_price' => 2.50,
-                'discount' => 2.13
-            )
-        )
-    ),
-    array(
-        'name' => 'supid',
-        'icon' => 'fa-utensil-spoon',
-        'data' => array(
-            array(
-                'dish_name' => 'Rassolnik',
-                'dish_description' => 'supp, hapukoor, leib',
-                'dish_price' => 1.10,
-                'discount' => 0.94
-            )
-        )
-    )
-);
+require_once 'menus/menu.php';
 foreach ($courses as $course => $courseData){
+    $courseCardTmpl = new Template('course_card');
+    $courseCardHeaderTmpl = new Template('course_card_header');
+    $courseCardDataTmpl = new Template('course_card_data');
     $courseCardHeaderTmpl->set('course_name', $courseData['name']);
     $courseCardHeaderTmpl->set('course_icon', $courseData['icon']);
     $courseCardTmpl->set('course_card_header', $courseCardHeaderTmpl->parse());
-
     $courseCardDataTmpl->set('course_name', $courseData['name']);
-
     $courseDatalistTmpl = new Template('course_data_list');
-    foreach ($courseData['data'] as $dish => $dishData) {
-        $courseDatalistTmpl->set('dish_name', $dishData['dish_name']);
-        $courseDatalistTmpl->set('dish_description', $dishData['dish_description']);
-        $courseDatalistTmpl->set('dish_price', $dishData['dish_price']);
-        $courseDatalistTmpl->set('discount', $dishData['discount']);
+    foreach ($courseData['data'] as $dish => $dishData){
+        foreach ($dishData as $name=>$value){
+            $courseDatalistTmpl->set($name, $dishData[$name]);
+        }
         $courseCardDataTmpl->add('course_data_list', $courseDatalistTmpl->parse());
     }
     $courseCardTmpl->set('course_card_data', $courseCardDataTmpl->parse());
-
     $contentTmpl->add('course_cards', $courseCardTmpl->parse());
 }
 $mainTmpl->set('content', $contentTmpl->parse());
--- menu/menus/menu.php
+++ menu/menus/menu.php
@@ -1,58 +1,34 @@
 <?php
-$praed = array(
+$courses = array(
     array(
-        'nimetus' => 'Sealihapada ploomide ja aprikoosiga',
-        'kirjeldus' => 'sealihapada, lisand, salat, leib',
-        'hind' => 2.65),
+        'name' => 'praed',
+        'icon' => 'fa-utensils',
+        'data' => array(
+            array(
+                'dish_name' => 'Sealihapada ploomide ja aprikoosiga',
+                'dish_description' => 'sealihapada, lisand, salat, leib',
+                'dish_price' => 2.65,
+                'discount' => 2.25
+            ),
+            array(
+                'dish_name' => 'Praetud kanakints',
+                'dish_description' => 'praetud kana, lisand, salat, leib',
+                'dish_price' => 2.50,
+                'discount' => 2.13
+            ),
+        )
+    ),
     array(
-        'nimetus' => 'Praetud kanakints',
-        'kirjeldus' => 'praetud kana, lisand, salat, leib',
-        'hind' => 2.50),
-    array(
-        'nimetus' => 'Hakklihakaste',
-        'kirjeldus' => 'hakklihakaste, lisand, salat, leib',
-        'hind' => 2.45),
-    array(
-        'nimetus' => 'Kartul, kaste, salat, leib',
-        'kirjeldus' => '',
-        'hind' => 1.38),
-    array(
-        'nimetus' => 'Hakklihakaste 1/2',
-        'kirjeldus' => 'hakklihakaste, lisand, salat, leib',
-        'hind' => 1.30)
-);
-$supid = array(
-    array(
-        'nimetus' => 'Rassolnik',
-        'kirjeldus' => 'supp, hapukoor, leib',
-        'hind' => 1.10),
-    array(
-        'nimetus' => 'Borss',
-        'kirjeldus' => 'supp, hapukoor, leib',
-        'hind' => 1.30)
-);
-$magusad = array(
-    array(
-        'nimetus' => 'Rosinakisell vahukoorega',
-        'kirjeldus' => '',
-        'hind' => 1.05),
-);
-$joogid = array(
-    array(
-        'nimetus' => 'Mahl',
-        'kirjeldus' => '',
-        'hind' => 0.60),
-    array(
-        'nimetus' => 'Morss',
-        'kirjeldus' => '',
-        'hind' => 0.25),
-    array(
-        'nimetus' => 'Piim',
-        'kirjeldus' => '',
-        'hind' => 0.30),
-    array(
-        'nimetus' => 'Keefir',
-        'kirjeldus' => '',
-        'hind' => 0.39)
+        'name' => 'supid',
+        'icon' => 'fa-utensil-spoon',
+        'data' => array(
+            array(
+                'dish_name' => 'Rassolnik',
+                'dish_description' => 'supp, hapukoor, leib',
+                'dish_price' => 1.10,
+                'discount' => 0.94
+            )
+        )
+    )
 );
 ?>
